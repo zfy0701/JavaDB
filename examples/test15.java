a4fa4663c73dd8b3473b3e438cfc09a5d8a7440b
zfy0701 <zfy0701@gmail.com>
2012-08-30 02:50:04 -0400

    renameing

---
+++ examples/test15.java
@@ -0,0 +1,29 @@
+//package examples;
+
+public class B {
+	public int f = 1;
+	public int foo() { return 0; };
+}
+
+public class A<T> with [int B.fname] {
+	B b = new B();
+
+	void test() {
+		//Object o = 3;
+		//int x = (Integer)o;
+		
+		//Integer i = 3;
+		//int x = i;
+		System.out.println("test value = " + b.fname);
+		b.fname = b.fname * 2;
+//		System.out.println("test value = " + b.funname());
+	}
+
+	public static void main(String []args) {
+		A a = new A<B> with [B.f] ();
+//		a.test();
+		
+		// A a2 = new A<B> with [B.foo] ();
+		// a2.test();
+	}
+}
