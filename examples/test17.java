97b6af27b793770883f27a661c2628a7763a4124
zfy0701 <zfy0701@gmail.com>
2012-09-07 16:01:53 -0400

    fix typechecker for transitive parmaters

---
+++ examples/test17.java
@@ -0,0 +1,30 @@
+//package examples;
+
+public class B {
+	public int x = 1;
+	public int foo() { return 0; };
+		public String foo2() { return 0; };
+
+}
+
+public class C with [int B.f, int B.g()] {
+	void test() {
+		B b = new B();
+		System.out.println("test value = " + b.f);
+
+		System.out.println("test value = " + b.g());
+	}
+}
+
+public class A with [int B.f, int B.g()] {
+
+	void test() {
+		C c = new C with [B.f, B.g] ();
+	}
+
+	public static void main(String []args) {
+		A a = new A with [B.x, B.foo] ();
+	//  A a = new A with [B.x, B.foo2] ();  //error
+		a.test();
+	}
+}
